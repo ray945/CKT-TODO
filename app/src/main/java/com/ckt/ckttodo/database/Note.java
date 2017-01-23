611193437da1efba818d8f643afa5e9f681b38c0
kai.cao <kai.cao@ck-telecom.com>
2017-01-23 01:52:29 -0800

    Init Database

---
+++ app/src/main/java/com/ckt/ckttodo/database/Note.java
@@ -0,0 +1,71 @@
+package com.ckt.ckttodo.database;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Note extends RealmObject {
+    //便签id
+    @PrimaryKey
+    private String noteId;
+    //便签标题
+    @Required
+    private String noteTitle;
+    //便签内容
+    @Required
+    private String noteContent;
+    //便签创建时间
+    private long noteCreateTime;
+    //便签更新时间
+    private long noteUpdateTime;
+    //便签创建人
+    private String userId;
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(String noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public long getNoteCreateTime() {
+        return noteCreateTime;
+    }
+
+    public void setNoteCreateTime(long noteCreateTime) {
+        this.noteCreateTime = noteCreateTime;
+    }
+
+    public long getNoteUpdateTime() {
+        return noteUpdateTime;
+    }
+
+    public void setNoteUpdateTime(long noteUpdateTime) {
+        this.noteUpdateTime = noteUpdateTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
---
+++ app/src/main/java/com/ckt/ckttodo/database/Plan.java
@@ -0,0 +1,126 @@
+package com.ckt.ckttodo.database;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Plan extends RealmObject {
+
+    /**
+     * 计划状态 status
+     * 未开始：PLAN_NOT_START
+     * 开始：PLAN_START
+     * 等待:PLAN_PENDING
+     * 阻塞:PLAN_BLOCK
+     * 完成:DONE
+     * */
+    public static final int PLAN_NOT_START = -1;
+    public static final int PLAN_START = 1;
+    public static final int PLAN_PENDING = 2;
+    public static final int PLAN_BLOCK = 3;
+    public static final int DONE = 4;
+
+    //计划id
+    @PrimaryKey
+    private String planId;
+    //计划名称
+    @Required
+    private String planName;
+    //计划创建人id
+    private String userId;
+    //计划创建时间
+    private long createTime;
+    //计划最新更新时间
+    private long lastUpdateTime;
+    //计划状态
+    private int status = PLAN_NOT_START;
+    //计划进度
+    private int accomplishProgress;
+    //计划预计时间
+    private float predictSpendTime;
+    //计划实际花费时间
+    private float realSpendTime;
+    //计划包含的任务
+    private RealmList<EventTask> eventTasks;
+
+    public String getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+
+    public long getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(long lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getAccomplishProgress() {
+        return accomplishProgress;
+    }
+
+    public void setAccomplishProgress(int accomplishProgress) {
+        this.accomplishProgress = accomplishProgress;
+    }
+
+    public float getPredictSpendTime() {
+        return predictSpendTime;
+    }
+
+    public void setPredictSpendTime(float predictSpendTime) {
+        this.predictSpendTime = predictSpendTime;
+    }
+
+    public float getRealSpendTime() {
+        return realSpendTime;
+    }
+
+    public void setRealSpendTime(float realSpendTime) {
+        this.realSpendTime = realSpendTime;
+    }
+
+    public RealmList<EventTask> getEventTasks() {
+        return eventTasks;
+    }
+
+    public void setEventTasks(RealmList<EventTask> eventTasks) {
+        this.eventTasks = eventTasks;
+    }
+}
