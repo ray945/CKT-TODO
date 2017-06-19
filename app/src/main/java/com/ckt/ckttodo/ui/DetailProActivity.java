609b52ff655297f832b319b0eaf81450a9834848
chengyang.hu <chengyang.hu@ck-telecom.com>
2017-06-18 23:23:50 -0700

    [Feature]Add Sprint spinner
    [Solution Description]
        N/A
    [Other Info]
        N/A

--- app/src/main/java/com/ckt/ckttodo/ui/DetailProActivity.java
+++ app/src/main/java/com/ckt/ckttodo/ui/DetailProActivity.java
@@ -8,8 +8,15 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.ckt.ckttodo.R;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by hcy on 17-5-25.
@@ -19,6 +26,7 @@ public class DetailProActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private Fragment mFragment;
     private TabLayout mTabLayout;
+    private Spinner mSpinner;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,12 @@ public class DetailProActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getResources().getString(R.string.personal_project));
         mTabLayout = (TabLayout) findViewById(R.id.detail_tab);
         mViewPager = (ViewPager) findViewById(R.id.detail_view_pager);
+        mSpinner = (Spinner) findViewById(R.id.nice_spinner);
+        List<String> items = new LinkedList<>(Arrays.asList("Sprint1", "Sprint2", "Sprint3", "Sprint4", "Sprint5"));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.simple_spinner_item, android.R.id.text1,  items);
+        arrayAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(arrayAdapter);
+
         FragmentPagerAdapter mFragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
